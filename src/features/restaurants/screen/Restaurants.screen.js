a7435aa49da5dc22aa2e7122fe57d99d36311270
experiencei <ayelojahighbee01@gmail.com>
2021-11-04 13:06:15 +0100

    working on restaurants card components

--- src/features/restaurants/screen/Restaurants.screen.js
+++ src/features/restaurants/screen/Restaurants.screen.js
@@ -35,19 +35,3 @@ const RestaurantsListContainer = styled.View`
     padding: 16px ;
     background-color: blue;
 `;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
