0f660815524df1ac53a4dff2b5259f5473b6e862
experiencei <ayelojahighbee01@gmail.com>
2021-11-04 11:07:19 +0100

    setting up eslint and prettier for the app

--- App.js
+++ App.js
@@ -6,14 +6,7 @@ import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen/>
       <ExpoStatusBar style="auto" />
     </>
   );
