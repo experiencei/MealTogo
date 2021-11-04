6b73ca8903073e1197e90615db7d1fe4f901249d
experiencei <ayelojahighbee01@gmail.com>
2021-11-04 11:31:53 +0100

    setting up eslint and prettier for the app

--- src/features/restaurants/components/Restaurant-info.component.js
+++ src/features/restaurants/components/Restaurant-info.component.js
@@ -4,12 +4,15 @@ import { Text } from "react-native";
 
 function RestaurantInfo({restaurant}) {
     const {
-        name ,
-        icon ,
-        photos ,
-        address ,
-        openingHours ,
-        isClosedTemporarily
+        name = "Some Restaurant",
+        icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+        photos = [
+          "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+        ],
+        address = "100 some random street",
+        isOpenNow = true,
+        rating = 4,
+        isClosedTemporarily = true,
     } = restaurant;
     return (
         <Text> RestaurantInfo </Text>
