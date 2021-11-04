4a7a7b7af07f7ecd4d47c00de737711ec20a2499
experiencei <ayelojahighbee01@gmail.com>
2021-11-04 13:02:19 +0100

    setting up eslint and prettier for the app

--- src/features/restaurants/screen/Restaurants.screen.js
+++ src/features/restaurants/screen/Restaurants.screen.js
@@ -36,3 +36,18 @@ const RestaurantsListContainer = styled.View`
     background-color: blue;
 `;
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
