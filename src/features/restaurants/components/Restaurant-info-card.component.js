5713ce159de389a56f2ca1193aae1ac3c22d6a1b
experiencei <ayelojahighbee01@gmail.com>
2021-11-05 17:01:50 +0100

    working on restaurant styles component to render a great UI

--- src/features/restaurants/components/Restaurant-info-card.component.js
+++ src/features/restaurants/components/Restaurant-info-card.component.js
@@ -3,51 +3,21 @@ import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 
-import { Card } from "react-native-paper";
-import styled from "styled-components/native";
 import { Spacer } from "../../../components/spacer/spacer.components";
 import { Text } from "../../../components/typography/text.component";
 
 
+import {
+    RestaurantCard,
+    RestaurantCardCover,
+    Info,
+    Section,
+    SectionEnd,
+    Rating,
+    Icon,
+    Address,
+  } from "./restaurant-info-card.styles";
 
-const Address =styled(Text)`
-   font-family :  ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`
-
-const RestaurantCard = styled(Card)`
-    background-color : ${(props) => props.theme.colors.bg.primary};
-`;
-
-const  RestaurantCardCover = styled(Card.Cover)`
-   padding :${(props) => props.theme.space[3]};
-   background-color: ${(props) => props.theme.colors.ui.primary};
-`
-const Info = styled.View`
-   padding: ${(props) => props.theme.space[3]};
-`;
-
-const Rating = styled.View`
-   flex-direction:row;
-   padding-top: ${(props) => props.theme.space[2]};
-   padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-
-const Icon = styled.Image`
-  width: 15px ;
-  height: 15px;
-`
 
 function RestaurantInfoCard({restaurant = {}}) {
     const {
