24c0a9ecd00a7a9a40764345a70d0a98c2e2455b
experiencei <ayelojahighbee01@gmail.com>
2021-11-05 12:08:40 +0100

    using expo google fonts to style fonts

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import {
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 // import { theme } from "./src/infrastructure/theme";
 import RestaurantsScreen from "./src/features/restaurants/screen/Restaurants.screen";
 import { theme } from "./src/infrastructure/theme";
