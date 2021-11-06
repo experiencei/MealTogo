067f60a6d39dd4f5862627ac8089483152a986d4
experiencei <ayelojahighbee01@gmail.com>
2021-11-06 13:20:01 +0100

    working on context api

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -10,7 +10,9 @@ export const restaurantRequest = ( location = "37.7749295,-122.4194155") => {
 
         resolve(mock)
     })
-}
+} 
+
+
 export const restaurantsTransform = ({ results = [] }) => {
     const mappedResults = results.map((restaurant) => {
       return {
