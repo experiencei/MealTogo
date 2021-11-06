96c82df9bcbf4b2feddc76aae0fbd08e40af49b0
experiencei <ayelojahighbee01@gmail.com>
2021-11-06 01:02:00 +0100

    adding icon as to implemenent navigation in the apps

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 
 import {
