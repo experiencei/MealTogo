878fafcad95cfad48cece27c67f2ccbc8ac2f7c9
experiencei <ayelojahighbee01@gmail.com>
2021-11-06 01:12:58 +0100

    adding react navigation to the app

--- src/features/restaurants/screen/Restaurants.screen.js
+++ src/features/restaurants/screen/Restaurants.screen.js
@@ -2,7 +2,8 @@ import React from "react";
 import styled from "styled-components/native";
 
 import { Searchbar } from "react-native-paper";
-import { StatusBar, SafeAreaView, FlatList } from "react-native";
+import {  FlatList } from "react-native";
+import { SafeArea } from "../../../components/utility/safe-area.component";
 import RestaurantInfoCard from "../components/Restaurant-info-card.component";
 
 function RestaurantsScreen() {
@@ -24,10 +25,6 @@ function RestaurantsScreen() {
 
 export default RestaurantsScreen;
 
-const SafeArea = styled(SafeAreaView)`
-   flex: 1;
-  ${StatusBar.currentHeight && ` margin-top: ${StatusBar.currentHeight}px `} ;
-`;
 const SearchContainer = styled.View`
   padding : ${(props) => props.theme.space[3]};
 `;
