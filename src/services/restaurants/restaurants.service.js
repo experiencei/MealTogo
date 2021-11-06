f78e2e1d94bc701d9ad5404b499cd6c507125536
experiencei <ayelojahighbee01@gmail.com>
2021-11-06 11:57:42 +0100

    using camelize to camelize the under score data from mock api

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,5 @@
 import {mocks} from "./mock";
+import camelize from "camelize";
 
 const restaurantRequest = ( location = "37.7749295,-122.4194155") => {
     return new Promise((resolve, reject) => {
@@ -11,6 +12,12 @@ const restaurantRequest = ( location = "37.7749295,-122.4194155") => {
     })
 }
 
-restaurantRequest().then((result) => {
+const restaurantTransform = (result) => {
+    return camelize(result)
+}
+
+restaurantRequest()
+.then(restaurantTransform)
+.then((transformedData) => {
     console.log();
 }).catch((err) => {console.log(err);})
