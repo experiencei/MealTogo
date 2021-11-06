16878c570aa8d8a534e620819d22e2b21bcce785
experiencei <ayelojahighbee01@gmail.com>
2021-11-06 23:24:49 +0100

    working on services that render the geocoding

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -10,14 +10,21 @@ const SearchContainer = styled.View`
 `;
 
 export const Search = () => {
-    
+
     const { keyword, search } = useContext(LocationContext);
     const [searchKeyword, setSearchKeyword] = useState(keyword);
 
     <SearchContainer>
     
     <Searchbar 
-        placeholder="Search for a location"
+       placeholder="Search for a location"
+        value={searchKeyword}
+        onSubmitEditing={() => {
+          search(searchKeyword);
+        }}
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+        }}
 
     />
     </SearchContainer>
