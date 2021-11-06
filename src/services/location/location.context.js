5d04e8bbf2a99ebc0bcfc1a5f6eb1661ceaaab8d
experiencei <ayelojahighbee01@gmail.com>
2021-11-06 22:52:12 +0100

    working on services that render the geocoding

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -3,4 +3,11 @@ import { locationRequest, locationTransform } from "./location.service";
 
 export const LocationContext = React.createContext();
 
-export const LocationContextProvider = ({ children }) => {}
+export const LocationContextProvider = ({ children }) => {
+    const [keyword, setKeyword] = useState("San Francisco");
+    const [location, setLocation] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+  
+
+}
