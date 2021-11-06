8c1c3f654632c484e61f6466de2a5a0b47834322
experiencei <ayelojahighbee01@gmail.com>
2021-11-06 14:05:47 +0100

    working on acdtivity indicators

--- src/features/restaurants/screen/Restaurants.screen.js
+++ src/features/restaurants/screen/Restaurants.screen.js
@@ -4,15 +4,32 @@ import { ActivityIndicator, Colors } from "react-native-paper";
 
 
 import { Searchbar } from "react-native-paper";
-import {  FlatList } from "react-native";
+import {  FlatList, View } from "react-native";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import RestaurantInfoCard from "../components/Restaurant-info-card.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { color } from "react-native-reanimated";
+
+const Loading = styled(ActivityIndicator)`
+   margin-left : --25px;
+`;
+
+const LoadingContainer = styled.View`
+   position: absolute;
+   top: 50%;
+   left: 50%;
+`;
 
 function RestaurantsScreen() {
   const { isLoading , error , restaurants} = useContext(RestaurantsContext)
   return (
+    
     <SafeArea>
+      {isLoading && (
+      <LoadingContainer>
+          <Loading size={50} animating={true} color={color.blue300}/>
+      </LoadingContainer>
+      )}
       <SearchContainer>
 
         <Searchbar />
