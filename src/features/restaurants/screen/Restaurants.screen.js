5fcfb769b2bbe053a2fa1880fbc75071e4039150
experiencei <ayelojahighbee01@gmail.com>
2021-11-06 23:14:18 +0100

    working on services that render the geocoding

--- src/features/restaurants/screen/Restaurants.screen.js
+++ src/features/restaurants/screen/Restaurants.screen.js
@@ -2,13 +2,12 @@ import React, { useContext } from "react";
 import styled from "styled-components/native";
 import { ActivityIndicator, Colors } from "react-native-paper";
 
-
-import { Searchbar } from "react-native-paper";
 import {  FlatList, View } from "react-native";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import RestaurantInfoCard from "../components/Restaurant-info-card.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { color } from "react-native-reanimated";
+import { Search } from "../components/search.component";
 
 const Loading = styled(ActivityIndicator)`
    margin-left : --25px;
