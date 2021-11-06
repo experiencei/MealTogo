dd929cb89ac025e6e6572ad594abfc10aba89009
experiencei <ayelojahighbee01@gmail.com>
2021-11-06 12:45:11 +0100

    working on context api

--- src/features/restaurants/screen/Restaurants.screen.js
+++ src/features/restaurants/screen/Restaurants.screen.js
@@ -5,16 +5,17 @@ import { Searchbar } from "react-native-paper";
 import {  FlatList } from "react-native";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import RestaurantInfoCard from "../components/Restaurant-info-card.component";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
 function RestaurantsScreen() {
-  const restau
+  const restaurantContext = useContext(RestaurantsContext)
   return (
     <SafeArea>
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
       <FlatList 
-          data={[{name : 1}, {name : 2}, {name : 1}, {name : 2}]}
+          data={restaurantContext.restaurant}
           renderItem={() => <RestaurantInfoCard /> }
           keyExtractor={(item) => item.name}
           contentContainerStyle={{padding: 16}}
