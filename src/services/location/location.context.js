27378561652f7d13149b5d5fdd0ada1c48219c02
experiencei <ayelojahighbee01@gmail.com>
2021-11-06 22:50:46 +0100

    working on services that render the geocoding

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -1,2 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { locationRequest, locationTransform } from "./location.service";
+
+export const LocationContext = React.createContext();
+
+export const LocationContextProvider = ({ children }) => {}
