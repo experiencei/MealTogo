4e2f42d92295afcf8f74f0ce8ba0c3cdbbd2b216
experiencei <ayelojahighbee01@gmail.com>
2021-11-07 23:30:00 +0100

    working on intagrating google map

--- src/features/restaurants/screen/restaurant-detail.screen.js
+++ src/features/restaurants/screen/restaurant-detail.screen.js
@@ -12,12 +12,20 @@ export const RestaurantDetailScreen = ({ navigation, route }) => {
     const [dinnerExpanded, setDinnerExpanded] = useState(false);
     const [drinksExpanded, setDrinksExpanded] = useState(false);
 
-    <List.Accordion
-    title="Breakfast"
-    left={(props) => <List.Icon {...props} icon="bread-slice" />}
-    expanded={breakfastExpanded}
-    onPress={() => setBreakfastExpanded(!breakfastExpanded)}
-  >
+    <SafeArea>
+      <RestaurantInfoCard restaurant={restaurant} />
+      <ScrollView>
+        <List.Accordion
+          title="Breakfast"
+          left={(props) => <List.Icon {...props} icon="bread-slice" />}
+          expanded={breakfastExpanded}
+          onPress={() => setBreakfastExpanded(!breakfastExpanded)}
+        >
+          <List.Item title="Eggs Benedict" />
+          <Divider />
+          <List.Item title="Classic Breakfast" />
+        </List.Accordion>
+        <Divider />
 
 
   </List.Accordion>
