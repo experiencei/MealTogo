e72d189fac7adac28a2af1d10ca723d552c42e9d
experiencei <ayelojahighbee01@gmail.com>
2021-11-07 22:03:13 +0100

    working on app navigator

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,7 +1,7 @@
 import {mocks} from "./mock";
 import camelize from "camelize";
 
-
+ 
 export const restaurantRequest =( location = "37.7749295,-122.4194155") => {
 
     return new Promise((resolve, reject) => {
