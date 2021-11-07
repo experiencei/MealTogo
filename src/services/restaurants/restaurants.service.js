23066db25957e0e4ecf791a01337fd3fe00695fe
experiencei <ayelojahighbee01@gmail.com>
2021-11-07 09:19:22 +0100

    working on services that render the geocoding

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -17,6 +17,7 @@ export const restaurantsTransform = ({ results = [] }) => {
     const mappedResults = results.map((restaurant) => {
       return {
     ...restaurant,
+    address: restaurant.vicinity,
     isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
     isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
       }
