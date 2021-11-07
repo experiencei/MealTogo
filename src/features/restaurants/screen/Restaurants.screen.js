e319354f74e50985612bad9215f316718cd2c691
experiencei <ayelojahighbee01@gmail.com>
2021-11-07 22:31:44 +0100

    stacking up according to diff navigation

--- src/features/restaurants/screen/Restaurants.screen.js
+++ src/features/restaurants/screen/Restaurants.screen.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import styled from "styled-components/native";
 import { ActivityIndicator, Colors } from "react-native-paper";
 
-import {  FlatList, View } from "react-native";
+import {  FlatList, Pressable } from "react-native";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import RestaurantInfoCard from "../components/Restaurant-info-card.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
