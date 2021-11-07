61b42e8562f13f77ce734954cf25c2d0bae5aa1b
experiencei <ayelojahighbee01@gmail.com>
2021-11-07 11:06:15 +0100

    working on services that render the geocoding

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -6,3 +6,5 @@ export const SafeArea = styled(SafeAreaView)`
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
+
+
