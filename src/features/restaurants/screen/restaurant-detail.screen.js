1af30a6cc253c3ebc2cf4804198b027c7d708603
experiencei <ayelojahighbee01@gmail.com>
2021-11-07 23:22:06 +0100

    stacking up according to diff navigation

--- src/features/restaurants/screen/restaurant-detail.screen.js
+++ src/features/restaurants/screen/restaurant-detail.screen.js
@@ -1,4 +1,15 @@
 import React, { useState, useContext } from "react";
+import { List, Divider } from "react-native-paper";
 
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
+
+
+export const RestaurantDetailScreen = ({ navigation, route }) => {
+    <List.Accordion
+    title="Breakfast"
+    left={(props) => <List.Icon {...props} icon="bread-slice" />}
+    expanded={breakfastExpanded}
+    onPress={() => setBreakfastExpanded(!breakfastExpanded)}
+  ></List.Accordion>
+}
