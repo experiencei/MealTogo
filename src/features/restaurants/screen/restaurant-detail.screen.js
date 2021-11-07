fda5431038456836cae5b1d01c3c87a1ed15edd9
experiencei <ayelojahighbee01@gmail.com>
2021-11-07 23:26:36 +0100

    stacking up according to diff navigation

--- src/features/restaurants/screen/restaurant-detail.screen.js
+++ src/features/restaurants/screen/restaurant-detail.screen.js
@@ -7,6 +7,11 @@ import { SafeArea } from "../../../components/utility/safe-area.component";
 
 
 export const RestaurantDetailScreen = ({ navigation, route }) => {
+    const [breakfastExpanded, setBreakfastExpanded] = useState(false);
+    const [lunchExpanded, setLunchExpanded] = useState(false);
+    const [dinnerExpanded, setDinnerExpanded] = useState(false);
+    const [drinksExpanded, setDrinksExpanded] = useState(false);
+
     <List.Accordion
     title="Breakfast"
     left={(props) => <List.Icon {...props} icon="bread-slice" />}
