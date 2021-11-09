e148a520d71c6aa687c2e866573b41b7f2d2511c
experiencei <ayelojahighbee01@gmail.com>
2021-11-09 16:22:54 +0100

    working on the auth of the app

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "styled-components/native";
 import * as firebase from "firebase";
 
 
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeArea } from "./src/components/utility/safe-area.component";
