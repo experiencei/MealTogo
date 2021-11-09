484f8deea457bc49a4996107330a2b97c2c08855
experiencei <ayelojahighbee01@gmail.com>
2021-11-09 17:04:53 +0100

    working on the auth of the app

--- src/features/restaurants/screen/restaurant-detail.screen.js
+++ src/features/restaurants/screen/restaurant-detail.screen.js
@@ -1,8 +1,9 @@
 import React, { useState, useContext } from "react";
 import { List, Divider } from "react-native-paper";
-
+ 
 
 import { ScrollView } from "react-native";
+
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
