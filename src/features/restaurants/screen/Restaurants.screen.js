dfc57cc7f1b1b9143a90484ad732b86749afaba6
experiencei <ayelojahighbee01@gmail.com>
2021-11-11 23:42:06 +0100

    expo publish animation

--- src/features/restaurants/screen/Restaurants.screen.js
+++ src/features/restaurants/screen/Restaurants.screen.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 
-import styled from "styled-components/native";
+import styled from "styled-components/native";  
 
 import { ActivityIndicator, Colors } from "react-native-paper";
 
