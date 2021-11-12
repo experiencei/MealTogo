28a310c0e28ebfae96635e725a3b697e1b307791
experiencei <ayelojahighbee01@gmail.com>
2021-11-12 23:31:35 +0100

    expo publish animation

--- src/features/restaurants/screen/Restaurants.screen.js
+++ src/features/restaurants/screen/Restaurants.screen.js
@@ -15,6 +15,7 @@ import {  FlatList, Pressable  } from "react-native";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
 import RestaurantInfoCard from "../components/Restaurant-info-card.component";
+
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { color } from "react-native-reanimated";
 import { Search } from "../components/search.component";
