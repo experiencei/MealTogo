8a0df2822a1b2841b1027eb584da0baf99192620
experiencei <ayelojahighbee01@gmail.com>
2021-11-15 21:23:08 +0100

    integrating with the google maps api

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
 
+
 export const MapCallout = ({ restaurant }) => (
   <CompactRestaurantInfo isMap restaurant={restaurant} />
 );
