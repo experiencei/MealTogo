dbb7ff56c764e477e663b1e9c67d19857269cc6e
experiencei <ayelojahighbee01@gmail.com>
2021-11-15 21:18:30 +0100

    integrating with the google maps api

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,5 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import styled from "styled-components/native";
+
+
 import { Searchbar } from "react-native-paper";
 import { LocationContext } from "../../../services/location/location.context";
 
